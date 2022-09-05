c15d1a5856f32e0256de24401591c85fd0ee0df9
gengbiao <898142408@qq.com>
2022-09-05 18:08:25 +0800

    refactor: 个人信息编辑，样式布局调整，剥离图片替换组件 feat: 新增系统维护》企业信息维护功能

---
+++ src/components/PublicGlobal/SvReplaceImg.vue
@@ -0,0 +1,101 @@
+<!-- name: 单张图标展示与上传替换 -->
+<script setup lang="ts">
+import { defineProps, defineEmits } from 'vue';
+import basicPinia from '@/pinia/storagePinia';
+import { uploadImage } from '@/apis/user';
+import { ElMessage } from 'element-plus';
+import type { UploadRequestOptions } from 'element-plus';
+
+const props = defineProps({
+  edit: {
+    type: Boolean,
+    required: true
+  },
+  shape: {
+    type: String,
+    default: 'square'
+  },
+  src: {
+    type: String,
+    required: true
+  },
+  size: {
+    type: Number,
+    default: 90
+  }
+});
+const emits = defineEmits(['success']);
+const basicStore = basicPinia();
+const BASE_URL = basicStore.getBaseUrl();
+const slotFail = ref(Boolean(useSlots().fail)); // 判断是否使用了图片回退插槽
+// 图片上传
+const uploadImg = async (file: UploadRequestOptions) => {
+  try {
+    let formData = new FormData();
+    formData.append('file', file.file);
+    const { data } = await uploadImage(formData);
+    if (data) {
+      ElMessage.success(data.message);
+      emits('success', data.url);
+    } else {
+      ElMessage.success('上传失败');
+    }
+  } catch (error) {
+    ElMessage.success('上传失败');
+  }
+};
+</script>
+
+<template>
+  <div class="root">
+    <el-upload
+      :multiple="false"
+      :limit="1"
+      :disabled="!props.edit"
+      :show-file-list="false"
+      :http-request="uploadImg"
+      accept="image/*"
+    >
+      <el-avatar
+        :size="props.size"
+        :class="{ edit: props.edit }"
+        :src="`${BASE_URL}/${props.src}`"
+        :shape="props.shape"
+      >
+        <slot name="fail"></slot>
+        <svg v-if="!slotFail" class="icon svg-80" aria-hidden="true">
+          <use href="#icon-tupianquesun"></use>
+        </svg>
+      </el-avatar>
+    </el-upload>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.edit {
+  position: relative;
+  margin-bottom: 3px;
+}
+
+.edit:hover::after {
+  content: '点击上传';
+  display: block;
+  cursor: pointer;
+  position: absolute;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  background: rgba(0, 0, 0, 0.4);
+  color: white;
+  font-size: 13px;
+  font-weight: bold;
+  line-height: 30px;
+}
+</style>
+
+<script lang="ts">
+export default {
+  name: 'SvReplaceImg'
+};
+</script>
---
+++ src/views/SystemManagement/components/CompanyMaintain.vue
@@ -0,0 +1,198 @@
+<!-- name: 企业维护 -->
+<script setup lang="ts">
+import { saveCompanyInfo, getCompanyInfo } from '@/apis/user';
+import { ElMessage } from 'element-plus';
+import basicPinia from '@/pinia/storagePinia';
+import type { FormInstance } from 'element-plus';
+
+const editSwitch = ref(false); // 编辑动作
+const userInfo = basicPinia().userInfo;
+// 重置企业信息
+const resetInfo = () => {
+  companyInfo.value.cusName = initCompanyInfo.value.cusName;
+  companyInfo.value.logo = initCompanyInfo.value.logo;
+  companyInfo.value.backImg = initCompanyInfo.value.backImg;
+};
+
+// 校验
+const rulesRef = ref<FormInstance>(); // form 表单实例
+const rules = reactive({
+  cusName: [{ required: true, message: '请输入用户名', trigger: 'blur' }]
+});
+
+// 获取企业信息
+const initCompanyInfo = ref();
+const companyInfo = ref({
+  backImg: '', // 背景图
+  cusName: '', // 企业名称
+  logo: '' // 企业logo
+});
+const init = async () => {
+  try {
+    const { data, code } = await getCompanyInfo({
+      cusNo: userInfo.cusNo
+    });
+    if (code === 200) {
+      const { backImg, cusName, logo } = data.companyInfo;
+      initCompanyInfo.value = {
+        backImg,
+        cusName,
+        logo
+      };
+      companyInfo.value = data.companyInfo;
+    }
+  } catch (e) {
+    console.log(e);
+  }
+};
+init();
+
+// 保存企业信息
+const saveInfo = async () => {
+  if (rulesRef.value) {
+    try {
+      const res = await rulesRef.value.validate();
+      if (res && companyInfo.value) {
+        const { data, code } = await saveCompanyInfo(companyInfo.value);
+        if (code === 200) {
+          ElMessage.success('保存成功');
+          editSwitch.value = false;
+        } else {
+          ElMessage.error('保存失败');
+        }
+      }
+    } catch (e) {
+      ElMessage.error('保存失败');
+    }
+  } else {
+    return;
+  }
+};
+</script>
+
+<template>
+  <div class="info-box">
+    <div class="title mg-b-15">
+      <div class="left">
+        <UserFilled class="svg-14 mg-r-5" />
+        <span class="ft-s-13 ft-w-6">企业维护</span>
+      </div>
+      <div class="right">
+        <el-switch
+          v-model="editSwitch"
+          size="small"
+          inline-prompt
+          width="45px"
+          active-text="编辑"
+          inactive-text="编辑"
+          inactive-color="#999"
+        />
+      </div>
+    </div>
+    <div class="content">
+      <el-form
+        ref="rulesRef"
+        label-position="right"
+        label-width="100px"
+        :model="companyInfo"
+        :rules="rules"
+        status-icon
+      >
+        <el-form-item label="企业名称：" prop="userName">
+          <transition name="inOut" mode="out-in" appear>
+            <span class="pd-l-10" v-if="!editSwitch">{{
+              companyInfo.cusName
+            }}</span>
+            <el-input v-else v-model="companyInfo.cusName" clearable />
+          </transition>
+        </el-form-item>
+        <el-form-item label="企业logo：">
+          <sv-replace-img
+            :edit="editSwitch"
+            :src="companyInfo.logo"
+            :size="90"
+            @success="e => (companyInfo.logo = e)"
+          />
+        </el-form-item>
+        <el-form-item label="登录背景：">
+          <sv-replace-img
+            :edit="editSwitch"
+            :src="companyInfo.backImg"
+            :size="90"
+            @success="e => (companyInfo.backImg = e)"
+          />
+        </el-form-item>
+      </el-form>
+    </div>
+    <div class="footer" v-show="editSwitch">
+      <el-button @click="resetInfo" size="small">重置</el-button>
+      <el-button type="primary" @click="saveInfo" size="small">保存</el-button>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.info-box {
+  padding: 5px 8px;
+  .title {
+    @include box-size(100%, 35px);
+    @include flex(row, space-between, center);
+    border-bottom: 0.8px solid #dedede;
+
+    > .left {
+      @include flex(row, center, center);
+    }
+  }
+
+  .content {
+    .avatar {
+      position: relative;
+      margin-bottom: 3px;
+    }
+
+    .avatar.edit:hover::after {
+      content: '更换LOGO';
+      display: block;
+      cursor: pointer;
+      position: absolute;
+      top: 0;
+      left: 0;
+      bottom: 0;
+      right: 0;
+      background: rgba(0, 0, 0, 0.4);
+      color: white;
+      font-size: 12px;
+      line-height: 35px;
+    }
+  }
+
+  .footer {
+    @include flex(row, flex-end, center);
+    height: 45px;
+  }
+}
+
+@media screen and (min-width: 100px) {
+  .info-box {
+    width: 80%;
+  }
+}
+
+@media screen and (min-width: 1000px) {
+  .info-box {
+    width: 60%;
+  }
+}
+
+@media screen and (min-width: 1500px) {
+  .info-box {
+    width: 40%;
+  }
+}
+</style>
+
+<script lang="ts">
+export default {
+  name: 'CompanyMaintain'
+};
+</script>
