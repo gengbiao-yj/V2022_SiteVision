a04b33a16e7fdb43506388d58cbdd6c6ad911856
gengbiao <898142408@qq.com>
2022-09-06 12:25:12 +0800

    docs: 更新文档

--- src/plugin/ElementPlus/index.ts
+++ src/plugin/ElementPlus/index.ts
@@ -1,6 +1,7 @@
 import * as ElementPlusIconsVue from '@element-plus/icons-vue';
 import type { App } from 'vue';
 
+// 引入所有 icon 图标
 export function setupAllIcon(app: App) {
   for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
     app.component(key, component);
