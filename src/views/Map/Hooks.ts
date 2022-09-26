1d762d691ad5db13a890cfb772e3348772136826
gengbiao <898142408@qq.com>
2022-09-26 18:05:50 +0800

    feat: 地图增加控件

--- src/views/Map/Hooks.ts
+++ src/views/Map/Hooks.ts
@@ -1,10 +1,17 @@
 import { ref, Ref, onMounted } from 'vue';
-import mapboxgl from 'mapbox-gl';
+import mapboxgl, { Map } from 'mapbox-gl';
 import { basicStore } from '@/pinia';
 import { AMAP } from '@/plugin/Axios/config';
 import { amapIP } from '@/apis/amap';
 
-// IP 定位
+/*  变量
+------------------------------------------------ */
+const mapContainer = ref() as Ref<HTMLDivElement>; // 地图容器
+const map = ref() as Ref<Map>; // 地图实例
+const initZoom = 9.0; // 初始缩放等级
+
+/*  高德 IP 定位
+------------------------------------------------ */
 const IPPosition = async () => {
   try {
     const data = await amapIP();
@@ -21,10 +28,74 @@ const IPPosition = async () => {
   }
 };
 
+/*  控制类 - 缩放级别
+------------------------------------------------ */
+class ZoomControl {
+  _map: Map | undefined;
+  _container: HTMLDivElement;
+  constructor(m: Map, h: HTMLDivElement) {
+    this._map = m;
+    this._container = h;
+  }
+  onAdd(map: Map) {
+    this._map = map;
+    this._container.className = 'mapboxgl-ctrl mapboxgl-ctrl-zoom';
+    this._container.textContent = `缩放级别:${initZoom}`;
+    return this._container;
+  }
+
+  onRemove() {
+    if (this._container.parentNode) {
+      this._container.parentNode.removeChild(this._container);
+    }
+    this._map = undefined;
+  }
+}
+
+/*  显示比例尺
+------------------------------------------------ */
+const createScale = (map: Map) => {
+  const scale = new mapboxgl.ScaleControl({
+    maxWidth: 100,
+    unit: 'metric'
+  });
+  map.addControl(scale);
+};
+
+/*  显示缩放控制
+------------------------------------------------ */
+const createNav = (map: Map) => {
+  const nav = new mapboxgl.NavigationControl({
+    showCompass: false
+  });
+  map.addControl(nav, 'bottom-right');
+};
+
+/*  显示缩放级别
+------------------------------------------------ */
+const createZoom = (map: Map) => {
+  map.addControl(
+    new ZoomControl(map, document.createElement('div')),
+    'bottom-right'
+  );
+};
+
+/*  绑定地图缩放事件
+------------------------------------------------ */
+const mapOnZoomed = (map: Map) => {
+  map.on('zoomend', () => {
+    const zoom = map.getZoom().toFixed(2); // 获取缩放级别
+    document.getElementsByClassName('mapboxgl-ctrl-zoom')[0].innerHTML =
+      '缩放级别:' + zoom;
+  });
+  map.resize();
+};
+
+/*  初始化地图
+------------------------------------------------ */
 export function UseInitMap() {
-  // 声明变量
-  const mapConatiner = ref() as Ref<HTMLDivElement>;
-  const map = ref();
+  const _mapContainer = mapContainer; // 地图容器
+  const _map = map; // 地图实例
   // 初始化地图
   onMounted(async () => {
     await IPPosition();
@@ -33,8 +104,8 @@ export function UseInitMap() {
       centerLngLat: { lng, lat }
     } = AmapV3IP;
     mapboxgl.accessToken = AMAP.AMAP_ACCESSTOKEN;
-    map.value = new mapboxgl.Map({
-      container: mapConatiner.value, // container ID
+    _map.value = new mapboxgl.Map({
+      container: _mapContainer.value, // container ID
       style: {
         version: 8,
         name: 'Mapbox Streets',
@@ -57,7 +128,7 @@ export function UseInitMap() {
         ]
       },
       center: [lng, lat], // starting position [lng, lat]
-      zoom: 9, // starting zoom
+      zoom: initZoom, // starting zoom
       minZoom: 3,
       maxZoom: 17.3,
       maxBounds: new mapboxgl.LngLatBounds(
@@ -69,10 +140,15 @@ export function UseInitMap() {
       preserveDrawingBuffer: true,
       dragRotate: false
     });
+
+    createScale(_map.value);
+    createZoom(_map.value);
+    createNav(_map.value);
+    mapOnZoomed(_map.value);
   });
 
   return {
-    mapConatiner,
-    map
+    mapContainer: _mapContainer,
+    map: _map
   };
 }
--- src/views/Map/Map.vue
+++ src/views/Map/Map.vue
@@ -1,8 +1,18 @@
 <!-- name:地图主页 -->
 <script setup lang="ts">
 import { UseInitMap } from './Hooks';
+import { busOn } from '@/utils/hooks';
 
-const { mapConatiner } = UseInitMap();
+const { map, mapContainer } = UseInitMap();
+/*  菜单折叠展开，变更地图尺寸
+------------------------------------------------ */
+busOn('menuCollapse', (param: boolean) => {
+  // 容器宽度变化结束后再 resize 变更地图尺寸
+  let ti = setTimeout(() => {
+    map.value.resize();
+    clearTimeout(ti);
+  }, 300);
+});
 </script>
 <script lang="ts">
 export default {
@@ -11,12 +21,16 @@ export default {
 </script>
 
 <template>
-  <div class="map-root" ref="mapConatiner"></div>
+  <div class="map-root" ref="mapContainer"></div>
 </template>
 
 <style scoped lang="scss">
 .map-root {
   width: 100%;
   height: 100%;
+
+  &:deep(.mapboxgl-ctrl-logo) {
+    display: none !important;
+  }
 }
 </style>
